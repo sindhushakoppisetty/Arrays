041f1fb0de48a9fe5d12c02fa55fb8135842eff6
sindhushakoppisetty <96305586+sindhushakoppisetty@users.noreply.github.com>
2022-02-04 14:48:37 +0530

    Add files via upload

---
+++ 2d array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+ void main()
+{
+	int rs,cs ; 
+	printf("enter the row size & column size");
+	scanf("%d%d",&rs,&cs);
+	int a[rs][cs],i,j;
+	printf("enter the elements in the array");
+		for (i=0;i<rs;i++)
+	{
+
+	for (j=0;j<cs;j++)
+   {
+     scanf("%d",&a[i][j]);
+ 
+ }
+}
+for (i=0;i<rs;i++)
+{
+	for(j=0;j<cs;j++)
+{
+	printf("a[%d] [%d]==> %d",i,j,a[i][j]);
+}
+printf("\n");
+}
+}
+
+
+
